bd5c052c486be78941cdb2d4da2d4ee7621a8a3a
Joeri Sebrechts <joeri@sebrechts.net>
2024-09-14 16:08:58 +0200

    add more tests
    - textcontent and direct tests
    - norender mode
    - better accuracy of elapsed measurement

--- public/vanilla/index.js
+++ public/vanilla/index.js
@@ -38,6 +38,12 @@ customElements.define('x-tick-append', class extends HTMLElement {
     }
 });
 
+customElements.define('x-tick-textcontent', class extends HTMLElement {
+    connectedCallback() {
+        this.textContent = '. ';
+    }
+});
+
 const template = document.createElement('template');
 template.innerHTML = '<span>.</span> ';
 customElements.define('x-tick-template', class extends HTMLElement {
@@ -50,12 +56,14 @@ customElements.define('x-app', class extends HTMLElement {
 
     #container;
     #buffered = false;
+    #rendered = true;
+    #running = false;
 
     connectedCallback() {
         const buttonContainer = document.createElement('div');
         this.append(buttonContainer);
 
-        ['innerhtml', 'append', 'shadowed', 'shadowed-append', 'lit', 'template'].forEach(
+        ['innerhtml', 'append', 'shadowed', 'shadowed-append', 'lit', 'template', 'textcontent', 'direct'].forEach(
             element => {
                 const button = document.createElement('button');
                 button.textContent = element;
@@ -64,41 +72,69 @@ customElements.define('x-app', class extends HTMLElement {
                 buttonContainer.insertAdjacentText('beforeend', ' ');
             });
 
-        const cb = document.createElement('input');
-        cb.type = 'checkbox';
-        cb.id = 'buffered';
-        cb.oninput = (e) => this.#buffered = e.target.checked;
-        buttonContainer.appendChild(cb);
-        const cbLabel = document.createElement('label');
-        cbLabel.htmlFor = 'buffered';
-        cbLabel.textContent = 'buffered';
-        cbLabel.title = 'buffered in a document fragment before appending to the main DOM';
-        buttonContainer.appendChild(cbLabel);
+        this.addCheckbox(
+            buttonContainer, 
+            'buffered',
+            'buffered in a document fragment before appending to the main DOM',
+            (e) => this.#buffered = !!e.target.checked
+        );
+
+        this.addCheckbox(
+            buttonContainer, 
+            'norender',
+            'create the elements but don\'t add them to the document',
+            (e) => this.#rendered = !e.target.checked
+        )
 
         this.#container = document.createElement('div');
         this.appendChild(this.#container);
     }
 
     run(element) {
+        if (this.#running) return;
+        this.#running = true;
         this.#container.innerHTML = '';
-        const start = Date.now();
-        let count = 0;
-        const container = this.#buffered ? document.createDocumentFragment() : this.#container;
-        while (count < MAX_COUNT) {
-            const tick = document.createElement('x-tick-' + element);
-            container.append(tick);
-            count++;
-        }
-        if (this.#buffered) {
-            this.#container.append(container);
-        }
-        const p = document.createElement('p');
-        this.#container.insertBefore(p, this.#container.firstChild);
-        // wait until the browser completes the render to measure
-        requestAnimationFrame(() => {
-            const elapsed = Date.now() - start;
-            const ticksPerMs = Math.round(count / elapsed);
-            p.textContent = `${count} ${element} ${this.#buffered ? 'buffered' : ''} components in ${elapsed} ms (${ticksPerMs} ticks/ms)`;
-        });
+        // give the browser the chance to do housekeeping after clearing the container
+        setTimeout(() => {
+            const start = Date.now();
+            let count = 0;
+            const container = this.#buffered ? document.createDocumentFragment() : this.#container;
+            let tick;
+            while (count < MAX_COUNT) {
+                if (element === 'direct') {
+                    tick = document.createElement('span');
+                    tick.textContent = '. ';
+                } else {
+                    tick = document.createElement('x-tick-' + element);
+                }
+                if (this.#rendered) container.append(tick);
+                count++;
+            }
+            if (this.#buffered) {
+                this.#container.append(container);
+            }
+            // wait until the browser completes the render to measure
+            requestAnimationFrame(() => {
+                const elapsed = Date.now() - start;
+                const ticksPerMs = Math.round(count / elapsed);
+                const p = document.createElement('p');
+                p.textContent = `${count} ${element} ${this.#buffered ? 'buffered' : ''} components in ${elapsed} ms (${ticksPerMs} ticks/ms)`;
+                this.#container.insertBefore(p, this.#container.firstChild);
+                this.#running = false;
+            });
+        }, 10);
+    }
+
+    addCheckbox(container, id, title, handler) {
+        const cb = document.createElement('input');
+        cb.type = 'checkbox';
+        cb.id = id;
+        cb.onchange = handler;
+        container.appendChild(cb);
+        const cbLabel = document.createElement('label');
+        cbLabel.htmlFor = id;
+        cbLabel.textContent = id;
+        cbLabel.title = title;
+        container.appendChild(cbLabel);
     }
 });
